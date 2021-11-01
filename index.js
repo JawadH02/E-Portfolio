d856179dfd33fc0b4a769b9c36e3ce56e3b9aec3
JawadHanif <JawadHanif.002@gmail.com>
2021-11-01 15:08:42 -0400

    Compressed images

--- index.js
+++ index.js
@@ -56,3 +56,21 @@ function toggleModal() {
     document.body.classList += " modal--open";
 
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
